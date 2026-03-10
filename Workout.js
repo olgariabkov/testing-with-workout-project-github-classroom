bdd094f12f68f4b64073ef5ad3d2bfb26510fe01
olgariabkov <157686222+olgariabkov@users.noreply.github.com>
2026-03-10 11:58:17 +0200

    Break validation intentionally

--- Workout.js
+++ Workout.js
@@ -3,7 +3,7 @@ import { logger } from './logger.js';
 
 
 const createWorkout = username => {
-    if (typeof username !== 'string' || username.length < 4) {
+    if (typeof username !== 'string' || username.length > 4) {
         throw new Error("the parameter username is not valid");
     }
     return db('workouts').insert({ username });
