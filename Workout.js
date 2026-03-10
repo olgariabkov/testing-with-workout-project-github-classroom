ecd516c698ed25da13c309a885838d0b3325868f
olgariabkov <157686222+olgariabkov@users.noreply.github.com>
2026-03-10 12:31:51 +0200

    Break addE completely

--- Workout.js
+++ Workout.js
@@ -2,21 +2,21 @@ import { db } from './dbConnection.js';
 import { logger } from './logger.js';
 
 
-/*const createWorkout = username => {
+const createWorkout = username => {
     if (typeof username !== 'string' || username.length < 4) {
         throw new Error("the parameter username is not valid");
     }
-    return db('workouts_exercises').insert({ username });
-};*/
-
-const createWorkout = username => {
-    throw new Error("Broken intentionally");
+    return db('workouts').insert({ username });
 };
 
-const addExercise = (workoutId, exerciseName) => {
+/*const addExercise = (workoutId, exerciseName) => {
     const result = db('workouts_exercises').insert({ workoutId, exerciseName });
     logger.logInfo({ workoutId, exerciseName }, `Exercise ${exerciseName} added to workout ${workoutId}`);
     return result;
+};*/
+
+const addExercise = () => {
+    return;
 };
 
 export { createWorkout, addExercise };
