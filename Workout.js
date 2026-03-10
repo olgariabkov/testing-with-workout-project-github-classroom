50ca73a396de5317d9ddccbf1c89f54002789d88
olgariabkov <157686222+olgariabkov@users.noreply.github.com>
2026-03-10 12:08:53 +0200

    Break createWorkout by inserting into wrong table

--- Workout.js
+++ Workout.js
@@ -6,8 +6,7 @@ const createWorkout = username => {
     if (typeof username !== 'string' || username.length < 4) {
         throw new Error("the parameter username is not valid");
     }
-    //return db('workouts').insert({ username });
-    return null;
+    return db('workouts_exercises').insert({ username });
 };
 
 const addExercise = (workoutId, exerciseName) => {
